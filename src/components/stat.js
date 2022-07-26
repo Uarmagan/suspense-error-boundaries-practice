e243bdd5cb185a3ff655984ae3dbcc988b4ad4f2
Ugur Armagan <ugurarmagan93@gmail.com>
2022-07-25 23:30:19 -0500

    ballin

--- src/components/stat.js
+++ src/components/stat.js
@@ -4,7 +4,7 @@ import Error from './error';
 import Spinner from './spinner';
 
 export default function Stat({ Icon, label, endpoint }) {
-  let { data, error } = useSWR(endpoint);
+  let { data, error } = useSWR(endpoint, { sus });
 
   return data ? (
     <div className='flex items-center w-full'>
